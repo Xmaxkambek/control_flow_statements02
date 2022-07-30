8e8de5b2a025f4f84c5a3cfd6fb2e8c27557a300
maxkambek <maxkambekxolbekov515@gmail.com>
2022-07-30 12:04:17 +0500

    03

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,18 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+
+int func(int c1, int c2, int c3) {
+  if (c1 > c2) {
+    if (c1 < c3) ;
+    return c1;
+  } else if (c2 > c1) {
+    if (c2 < c3) ;
+    return c2;
+  }
+  return c3;
+}
+
 void main() {
+  print(func(6, 5, 7));
 }
