1035e3b50023b58ecc8d493372f4fae2b18c1afd
maxkambek <maxkambekxolbekov515@gmail.com>
2022-07-30 11:56:56 +0500

    01

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,22 @@ Args:
 Returns:
     int: return answer.
 */
+
+int func(int b1, int b2, int b3) {
+  int b = 1;
+  if (b1 > b2) {
+    if (b1 > b3) ;
+    b = b1;
+  } else if (b2 > b1) {
+    if (b2 > b3) ;
+    b = b2;
+  } else if (b3 > b1) {
+    if (b3 > b2) ;
+    b = b3;
+  }
+  return b;
+}
+
 void main() {
+  print(func(9, 2, 8));
 }
