669d2149b52f0ed0183abf77e5740c03aca186f3
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-01 11:45:57 +0500

    6

--- bin/if06.dart
+++ bin/if06.dart
@@ -1,10 +1,50 @@
 /* 
 Create function called func
-Find the index of the largest digit of the five-digit number.
+Find the index of the largest digit of the four-digit number.
 Args:
     n: Five-digit number.
 Returns:
-    int: return answer. 
-*/
+    int: return answer. */
+
+int func(int a) {
+  int a = 6547;
+  int x1, x2, x3, x4;
+  x1 = a % 10;
+  a ~/= 10;
+  x2 = a % 10;
+  a ~/= 10;
+  x3 = a % 10;
+  a ~/= 10;
+  x4 = a % 10;
+  if (x1 > x2) {
+    if (x1 > x3) {
+      if (x1 > x4) {
+        return 1;
+      }
+    }
+  } else if (x2 > x1) {
+    if (x2 > x3) {
+      if (x2 > x4) {
+        return 2;
+      }
+    }
+  } else if (x3 > x1) {
+    if (x3 > x2) {
+      if (x3 > x4) {
+        return 3;
+      }
+    }
+  } else if (x4 > x1) {
+    if (x4 > x2) {
+      if (x4 > x3) {
+        return 4;
+      }
+    }
+  }
+
+  return x1;
+}
+
 void main() {
+  print(func(6547));
 }
